33ba90e209d6869a8bf34028833b9567b86e2abc
Manjeet-pandey <075bct048.manjeet@pcampus.edu.np>
2022-01-23 23:34:39 +0545

    Image Upload using react

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import ImageUpload from "./ImageUpload.js";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <ImageUpload />
     </div>
   );
 }
-
 export default App;
