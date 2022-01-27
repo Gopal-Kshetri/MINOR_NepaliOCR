ab5d24cafb173a7c3eb0dcd0638427b4398ea8a6
abishz17 <075bct008.abish@pcampus.edu.np>
2022-01-27 23:47:19 +0545

    Image Upload to sever

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import ImageUpload from "./ImageUpload.js";
 
-function App() {
-  return (
-    <div className="App">
-      <ImageUpload />
-    </div>
-  );
-}
+import React from 'react';
+import ImageUpload from './components/ImageUpload';
+
+const App = () => {
+  return(
+  <div className='container mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+      Upload yours Image
+    </h4>
+    <ImageUpload />
+  </div>
+  )
+};
+
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,39 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(fileUpload());
+
 app.get("/api", (req, res) => {
     res.json({ message: "Manoj muji ho" });
   });
+
+app.post("/upload",(req,res)=>{
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json({message:'No image uploaded'});
+  }
+
+  sampleFile = req.files.image;
+  uploadPath = __dirname + '/client/uploads/'+sampleFile.name;
+
+  sampleFile.mv(`./Nepali-OCR/${sampleFile.name}`,function(err){
+    if(err){
+      console.error(err);
+      return res.status(500).send(err);
+
+    }
+    
+    res.json({ fileName: sampleFile.name, filePath: `/uploads/${sampleFile.name}` });
+  });
+
+ 
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
